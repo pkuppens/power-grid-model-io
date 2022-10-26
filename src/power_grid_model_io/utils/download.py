f969a0cd020af48a4f26f6e712063a6c3dd2efc7
Bram Stoeller <bram.stoeller@alliander.com>
2022-10-26 11:57:58 +0200

    Use sha256 hashes instead of sha1
    Signed-off-by: Bram Stoeller <bram.stoeller@alliander.com>

--- src/power_grid_model_io/utils/download.py
+++ src/power_grid_model_io/utils/download.py
@@ -148,9 +148,9 @@ def get_download_path(dir_path: Optional[Path], file_path: Optional[Path], data=
     # If no file_path is given, generate a file name
     if file_path is None:
         try:
-            sha1 = hashlib.sha1()
-            sha1.update(data)
-            hash_str = sha1.hexdigest()
+            sha256 = hashlib.sha256()
+            sha256.update(data)
+            hash_str = sha256.hexdigest()
         except (TypeError, ValueError) as ex:
             raise ValueError(f"Can't auto generate a file name for a {type(data).__name__}.") from ex
         file_path = Path(__name__) / f"{hash_str}.download"
